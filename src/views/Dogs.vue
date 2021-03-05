3ef35c8d2851c846fcda4f1d82961de40c35ef91
Floturner <56768827+Floturner@users.noreply.github.com>
2021-03-05 16:46:50 +0000

    first commit

---
+++ src/views/Dogs.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <PetTable species="Dogs" :pets="dogs" />
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import PetTable from '@/components/PetTable.vue'
+
+export default {
+  components: {
+    PetTable
+  },
+  data () {
+    return {}
+  },
+  computed: {
+    ...mapState(['dogs'])
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,113 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home-view-container">
+    <h1>Adopt a new best friend.</h1>
+    Cats : {{ getAllCats.length }}
+    <br><br>
+
+    Dogs : {{ getAllDogs.length }}
+    <br><br>
+
+    All : {{ animalsCount }}
+    <br><br>
+
+    <button @click="togglePetForm" class="btn btn-primary">Add New Pet</button>
+    <br><br>
+
+    <b-form @submit.prevent="handleSubmit" v-if="showPetForm">
+      <b-form-group
+        id="input-group-1"
+        type="text"
+        label="Pet's Name :"
+        label-for="input-1"
+        >
+        <b-form-input
+          id="input-1"
+          v-model="formData.name"
+          placeholder="Enter pet's name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group
+        id="input-group-2"
+        label="Species :"
+        label-for="input-2"
+        >
+        <b-form-select
+          id="input-2"
+          v-model="formData.species"
+          :options="['cats', 'dogs']"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group
+        id="input-group-3"
+        label="Pet's Age :"
+        label-for="input-3"
+        >
+        <b-form-input
+          id="input-3"
+          type="number"
+          v-model="formData.age"
+          placeholder="Enter pet's age"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      showPetForm: false,
+      formData: {
+        name: '',
+        age: 0,
+        species: null
+      }
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'animalsCount',
+      'getAllCats',
+      'getAllDogs'
+    ])
+  },
+  methods: {
+    ...mapActions([
+      'addPet'
+    ]),
+    togglePetForm () {
+      this.showPetForm = !this.showPetForm
+    },
+    handleSubmit () {
+      const { species, age, name } = this.formData
+      const payload = {
+        species,
+        pet: {
+          name,
+          age
+        }
+      }
+      this.addPet(payload)
+
+      // reset fields
+      this.formData = {
+        name: '',
+        age: 0,
+        species: null
+      }
+    }
   }
 }
 </script>
